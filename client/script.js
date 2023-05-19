8cf9b6b246d2adb3ed9c1deb753793da7409a586
Shivbhan Kushwaha <108090221+ShivBhanKushWaha@users.noreply.github.com>
2023-05-19 17:17:01 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -49,7 +49,7 @@ function chatStripe(isAi, value, uniqueId) {
         `
         <div class="wrapper ${isAi && 'ai'}">
             <div class="chat">
-                <div class="profile">
+                <div class="profile element">
                     <img 
                       src=${isAi ? bot : user} 
                       alt="${isAi ? 'bot' : 'user'}" 
